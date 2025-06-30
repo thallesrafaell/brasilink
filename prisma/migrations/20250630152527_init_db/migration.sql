3eaf2a64efa60e255cde1b856ca015590aa3ce83
thalles <thallesrafaell@outlook.com>
2025-06-30 12:29:10 -0300

    feat: add Prisma schema and database migration files; configure Prisma client

---
+++ prisma/migrations/20250630152527_init_db/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "services" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "services_pkey" PRIMARY KEY ("id")
+);
