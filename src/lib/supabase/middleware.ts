b0f77ff79f010ac58a19176c06b28a3496d498ba
thalles <thallesrafaell@outlook.com>
2025-06-30 23:13:55 -0300

    feat: permitir acesso ao fluxo de login e autenticação antes de redirecionar

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -43,7 +43,8 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/email-verification")
+    !request.nextUrl.pathname.startsWith("/email-verification") &&
+    !request.nextUrl.pathname.startsWith("/user/")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
